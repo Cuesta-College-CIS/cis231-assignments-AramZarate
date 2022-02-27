e569bbdd209ff684129a52299298d2e58dafc005
Kyuwoong Lee <kwooler@gmail.com>
2022-02-26 21:41:56 -0800

    Update LabProgram-2.java

--- LabProgram-2.java
+++ LabProgram-2.java
@@ -6,7 +6,7 @@ public class LabProgram {
      Scanner scnr = new Scanner (System.in);
         int Num;
       int max = 0;
-      int min = 0;
+      int min = 0; // Min is initailzed 0
       int total = 0;
       double average;
       int count = 0;
@@ -19,7 +19,7 @@ public class LabProgram {
             max = Num;
          }
          if (Num < min) {
-            min = Num;
+            min = Num;     // Never changed if all values are greater than 0. Thus min is 0 even if there is no 0's in input. It causes an Error.
          }
 
          total += Num;
@@ -27,9 +27,12 @@ public class LabProgram {
 
          Num = scnr.nextInt();
       }
-      average = (total-max-min) / (count-2);
+      average = (total-max-min) / (count-2);   // dividend / dividor  all values are integers. Thus, result will be integer. Your average will be always truncated. 
+      // You should use "type cast" before division operation.
+      
       int sum = total-max-min;
 
-      System.out.printf("%d %.2f",sum,average);
+      // System.out.printf("%d %.2f",sum,average);
+      System.out.printf("%d %.2f\n",sum,average);
    }
 }
