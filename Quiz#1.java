646e4e81f953bdb0983287b12febe09cce782bac
Kyu Lee <kwooler@gmail.com>
2022-04-05 18:59:11 -0700

    quiz121

--- Quiz#1.java
+++ Quiz#1.java
@@ -1,97 +1,97 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class quiz1 {
-public static void main(String[] args)  {
-Scanner scnr = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
 
-int tmp;
-int N = scnr.nextInt();
-int[] numbers = new int[N];
-int minidx = 0;
-int minval = Integer.MAX_VALUE;//finds min of array
+        int tmp;
+        int N = scnr.nextInt();
+        int[] numbers = new int[N];
+        int minidx = 0;
+        int minval = Integer.MAX_VALUE;// finds min of array
 
-for(int i = 0;i < numbers. length; i++){
-numbers[i] = scnr.nextInt();
-}
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scnr.nextInt();
+        }
 
-for (int i = 0; i < numbers. length; i++){
-    if(minval > numbers[i]){ //goes through whole array to find min 
-    minval = numbers[i];
-    minidx = i;
+        for (int i = 0; i < numbers.length; i++) {
+            if (minval > numbers[i]) { // goes through whole array to find min
+                minval = numbers[i];
+                minidx = i;
+            }
+        }
+        tmp = numbers[0];
+        numbers[0] = numbers[minidx]; // sets temporary value for first value of array
+        numbers[minidx] = tmp;// swaps values
+
+        for (int i = 0; i < numbers.length; i++) { // prints array with swapped variables
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+
+        minval = Integer.MAX_VALUE;
+        for (int i = 1; i < numbers.length; i++) {
+            if (minval > numbers[i]) { // looks to find mn value after second value only
+                minval = numbers[i];
+                minidx = i;
+            }
+        }
+        tmp = numbers[1]; // swaps variables with second value of array
+        numbers[1] = numbers[minidx];
+        numbers[minidx] = tmp;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+
+        minval = Integer.MAX_VALUE;
+        for (int i = 2; i < numbers.length; i++) {
+            if (minval > numbers[i]) {
+                minval = numbers[i];
+                minidx = i;
+            }
+        }
+        tmp = numbers[2];
+        numbers[2] = numbers[minidx];
+        numbers[minidx] = tmp;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+
+        minval = Integer.MAX_VALUE;
+        for (int i = 3; i < numbers.length; i++) {
+            if (minval > numbers[i]) {
+                minval = numbers[i];
+                minidx = i;
+            }
+        }
+        tmp = numbers[3];
+        numbers[3] = numbers[minidx];
+        numbers[minidx] = tmp;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+
+        minval = Integer.MAX_VALUE;
+        for (int i = 4; i < numbers.length; i++) {
+            if (minval > numbers[i]) {
+                minval = numbers[i];
+                minidx = i;
+            }
+        }
+        tmp = numbers[4];
+        numbers[4] = numbers[minidx];
+        numbers[minidx] = tmp;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
     }
-    }
-tmp = numbers[0]; 
-numbers[0] = numbers[minidx]; //sets temporary value for first value of array
-numbers[minidx] = tmp;//swaps values 
-
-for (int i = 0; i < numbers.length; i++){ //prints array with swapped variables
-System.out.print(numbers[i] + " ");
 }
-System.out.println();
-
- minval = Integer.MAX_VALUE;
-for (int i = 1; i < numbers. length; i++){
-if(minval > numbers[i]){ //looks to find mn value after second value only
-minval = numbers[i];
-minidx = i;
-}
-}
-tmp = numbers[1]; //swaps variables with second value of array
-numbers[1] = numbers[minidx];
-numbers[minidx] = tmp;
-
-for (int i = 0; i < numbers.length; i++){
-System.out.print(numbers[i] + " ");
-}
-System.out.println();
-
- minval = Integer.MAX_VALUE;
-for (int i = 2; i < numbers.length; i++){
-if(minval > numbers[i]){
-minval = numbers[i];
-minidx = i;
-}
-}
-tmp = numbers[2];
-numbers[2] = numbers[minidx];
-numbers[minidx] = tmp;
-
-for (int i = 0; i < numbers.length; i++){
-System.out.print(numbers[i] + " ");
-}
-System.out.println();
-
- minval = Integer.MAX_VALUE;
-for (int i = 3; i < numbers.length; i++){
-if(minval > numbers[i]){
-minval = numbers[i];
-minidx = i;
-}
-}
-tmp = numbers[3];
-numbers[3] = numbers[minidx];
-numbers[minidx] = tmp;
-
-for (int i = 0; i < numbers.length; i++){
-System.out.print(numbers[i] + " ");
-}
-System.out.println();
-
- minval = Integer.MAX_VALUE;
-for (int i = 4; i < numbers.length; i++){
-if(minval > numbers[i]){
-minval = numbers[i];
-minidx = i;
-}
-}
-tmp = numbers[4];
-numbers[4] = numbers[minidx];
-numbers[minidx] = tmp;
-
-for (int i = 0; i < numbers.length; i++){
-System.out.print(numbers[i] + " ");
-}
-
-
-}}
-
--- Quiz#2.java
+++ Quiz#2.java
@@ -2,26 +2,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class quiz2 {
-	public static void main(String[] args) {
-		Scanner scnr = new Scanner(System.in);
-		final int L = 3; // will NOT use L. Just for the extendability.
-		final int M = 3;
-		final int N = 2;
-		int matrix1[][]={{2, 5, 10},{3, 6, 9},{1, 2, 3}};  
-        int matrix2[][]={{10, 20},{30, 40},{50, 60}};  
-     int rowidx = scnr.nextInt();
- 
-    int c[][]=new int[3][2];  //creates result matrix
-  
-    for(int i=0;i<3;i++){  
-        for(int j=0;j<2;j++){  
-            c[i][j]=0;    
-        for(int k=0;k<3;k++){    
-            c[i][j]+=matrix1[i][k]*matrix2[k][j];    //multiplies each row with each other 
-}
-}
-}  
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int L = 3; // will NOT use L. Just for the extendability.
+        final int M = 3;
+        final int N = 2;
+        int matrix1[][] = { { 2, 5, 10 }, { 3, 6, 9 }, { 1, 2, 3 } };
+        int matrix2[][] = { { 10, 20 }, { 30, 40 }, { 50, 60 } };
+        int rowidx = scnr.nextInt();
 
-for(int j = 0; j < c[rowidx].length ; j++){ //gets user input to print out specific row result
-   System.out.print(c[rowidx][j]+" ");
-}}}
+        int c[][] = new int[3][2]; // creates result matrix
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    c[i][j] += matrix1[i][k] * matrix2[k][j]; // multiplies each row with each other
+                }
+            }
+        }
+
+        for (int j = 0; j < c[rowidx].length; j++) { // gets user input to print out specific row result
+            System.out.print(c[rowidx][j] + " ");
+        }
+    }
+}
